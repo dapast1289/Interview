6003ee9bace6633adab0c6da107e5169157f0718
leochen <dapast1289@gmail.com>
2022-06-04 15:19:43 +0100

    update

--- src/main/java/leetcode/L056.java
+++ src/main/java/leetcode/L056.java
@@ -20,96 +20,91 @@ public class L056 {
         if (intervals.length == 1) {
             return intervals;
         }
-        int[][] arr = copy(intervals);
 
         //sort
-        mergeSort(arr, 0, arr.length - 1);
+        mergeSort(intervals, 0, intervals.length - 1);
+
         //compare
-        return arr;
+        int i = 0;
+        int count = 0;
+        for (int j = 1; j < intervals.length; j++) {
+            int pivot = intervals[i][1];
+            if (pivot >= intervals[j][0]) {
+                intervals[j][0] = -1;
+                count++;
+                if (pivot >= intervals[j][1]) {
+                    intervals[j][1] = -1;
+                    count++;
+                } else {
+                    intervals[i][1] = -1;
+                    i = j;
+                    count++;
+                }
+            }
+        }
+
+        // generate result
+        // int[][] result = new int[(intervals.length - count/2)][2];
+        // int ri = 0, rj = 0;
+        // for (int[] nums : intervals) {
+        //     for (int num : nums) {
+        //         if (num != -1) {
+        //             result[ri][rj] = num;
+        //             rj++;
+        //             if (rj == 2) {
+        //                 ri++;
+        //                 rj = 0;
+        //             }
+        //         }
+        //     }
+        // }
+        return intervals;
     }
 
     public void mergeSort(int[][] arr, int left, int right) {
         if (left < right) {
-            int mid = (right + left) / 2;
+            int mid = (right + left ) / 2;
             mergeSort(arr, left, mid);
             mergeSort(arr, mid + 1, right);
             merge(arr, left, mid, right);
-            print(arr);
         }
     }
 
     public void merge(int[][] arr, int left, int mid, int right) {
         int[][] result = new int[right - left + 1][2];
-        int i1 = left;
-        int j1 = 0;
-        int i2 = mid + 1;
-        int j2 = 0;
-        int i3 = 0;
-        int j3 = 0;
-        while (i1 <= mid && i2 <= right) {
-            if (arr[i1][j1] <= arr[i2][j2]) {
-                result[i3][j3] = arr[i1][j1];
-                j1++;
+        int l1 = left;
+        int l2 = mid + 1;
+        int l3 = 0;
+        while (l1 <= mid && l2 <= right) {
+            if (arr[l1][0] <= arr[l2][0]) {
+                result[l3][0] = arr[l1][0];
+                result[l3][1] = arr[l1][1];
+                l1++;
             } else {
-                result[i3][j3] = arr[i2][j2];
-                j2++;
+                result[l3][0] = arr[l2][0];
+                result[l3][1] = arr[l2][1];
+                l2++;
             }
-            j3++;
-            if (j1 == 2) {
-                j1 = 0;
-                i1++;
-            }
-            if (j2 == 2) {
-                j2 = 0;
-                i2++;
-            }
-            if (j3 == 2) {
-                j3 = 0;
-                i3++;
-            }
+            l3++;
         }
-        // add remained value
-        while (i1 <= mid) {
-            result[i3][j3] = arr[i1][j1];
-            j1++;
-            j3++;
-            if (j1 == 2) {
-                j1 = 0;
-                i1++;
-            }
-            if (j3 == 2) {
-                j3 = 0;
-                i3++;
-            }
+        // deal with remained value
+        while (l1 <= mid) {
+            result[l3][0] = arr[l1][0];
+            result[l3][1] = arr[l1][1];
+            l1++;
+            l3++;
         }
-        while (i2 <= right) {
-            result[i3][j3] = arr[i2][j2];
-            j2++;
-            j3++;
-            if (j2 == 2) {
-                j2 = 0;
-                i2++;
-            }
-            if (j3 == 2) {
-                j3 = 0;
-                i3++;
-            }
+        while (l2 <= right) {
+            result[l3][0] = arr[l2][0];
+            result[l3][1] = arr[l2][1];
+            l2++;
+            l3++;
         }
         // replace
-        for (int[] ints : result) {
-            arr[left][0] = ints[0];
-            arr[left][1] = ints[1];
+        for (int i = 0; i < result.length; i++) {
+            arr[left][0] = result[i][0];
+            arr[left][1] = result[i][1];
             left++;
         }
-    }
-
-    public int[][] copy(int[][] intervals) {
-        int[][] arr = new int[intervals.length][2];
-        //copy intervals
-        for (int i = 0; i < intervals.length; i++) {
-            arr[i][0] = intervals[i][0];
-            arr[i][1] = intervals[i][1];
-        }
-        return arr;
     }
 }
