d435bd3a156433c1a1d952d180b35aec8bb13b29
leochen <dapast1289@gmail.com>
2022-06-12 17:17:07 +0100

    update

--- src/test/java/leetcode/L077.java
+++ src/test/java/leetcode/L077.java
@@ -1,27 +1,49 @@
 package leetcode;
 
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class L077 {
+    private static Stream<Arguments> arrayStream1() {
+        List<List<Integer>> expected = new LinkedList<>();
+        int[][] expected1In = {
+                {1, 2},
+                {1, 3},
+                {1, 4},
+                {2, 3},
+                {2, 4},
+                {3, 4}
+        };
+        for (int[] numList : expected1In) {
+            List<Integer> ans = new ArrayList<>();
+            for (int num : numList) {
+                ans.add(num);
+            }
+            expected.add(ans);
+        }
 
-    private static Stream<Arguments> arrayStream() {
         return Stream.of(
-
-        );
+                Arguments.of(4, 2, expected));
     }
 
-    public static List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> combs = new ArrayList<List<Integer>>();
-        combine(combs, new ArrayList<Integer>(), 1, n, k);
-        return combs;
+    @ParameterizedTest
+    @MethodSource("arrayStream1")
+    public void combine(int n, int k, List<List<Integer>> expected) {
+        List<List<Integer>> combs = new ArrayList<>();
+        combine(combs, new ArrayList<>(), 1, n, k);
+        assertEquals(expected, combs);
     }
-    public static void combine(List<List<Integer>> combs, List<Integer> comb, int start, int n, int k) {
+    public void combine(List<List<Integer>> combs, List<Integer> comb, int start, int n, int k) {
         if(k==0) {
-            combs.add(new ArrayList<Integer>(comb));
+            combs.add(new ArrayList<>(comb));
             return;
         }
         for(int i=start;i<=n;i++) {
