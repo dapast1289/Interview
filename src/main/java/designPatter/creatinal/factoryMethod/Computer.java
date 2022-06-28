f9568c3c80083f4a15ac1de5800183c7753d4820
leochen <dapast1289@gmail.com>
2022-06-28 01:23:03 +0100

    update

---
+++ src/main/java/designPatter/creatinal/factoryMethod/Computer.java
@@ -0,0 +1,14 @@
+package designPatter.creatinal.factoryMethod;
+
+public abstract class Computer {
+
+    public abstract String getRAM();
+
+    public abstract String getHDD();
+
+    public abstract String getCPU();
+
+    public String toString() {
+        return "RAM= " + this.getRAM() + ", HDD=" + this.getHDD() + ", CPU=" + this.getCPU();
+    }
+}
