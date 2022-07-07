863a8ab0a499848ba55b1e918dd4ca55676122f0
leochen <dapast1289@gmail.com>
2022-07-08 00:50:01 +0100

    update

--- src/main/java/Stegosaurus.java
+++ src/main/java/Stegosaurus.java
@@ -1,20 +1,36 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
-public class Stegosaurus {
-}
+public class Stegosaurus implements EggLayer<Stegosaurus>{
 
+    @Override
+    public Egg<Stegosaurus> lay() {
+        return new Egg<>(Stegosaurus::new);
+    }
+    public static void main(String[] args) throws Exception {
+        Stegosaurus s1 = new Stegosaurus();
+        Egg<Stegosaurus> egg1 = s1.lay();
+        Stegosaurus s2 = egg1.hatch();
+        Egg<Stegosaurus> egg2 = s2.lay();
+        egg2.hatch();
+        System.out.println();
+    }
+
+}
 interface EggLayer<T extends EggLayer<T>> {
     public Egg<T> lay();
 }
-
 class Egg<T extends EggLayer<T>> {
+    private boolean isHatch = false;
+    private final Supplier<T> supplier;
     public Egg(Supplier<T> supplier) {
+        this.supplier = supplier;
     }
-
-    public T hatch() {
-        return null;
+    public T hatch() throws Exception {
+        if (!this.isHatch) {
+            isHatch = true;
+            return supplier.get();
+        } else {
+            throw new Exception("");
+        }
     }
 }
