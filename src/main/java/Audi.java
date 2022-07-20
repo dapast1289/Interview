c3aa2c7b8558ea63afe553a62fb59eaa4400b7ec
leochen <dapast1289@gmail.com>
2022-07-20 22:48:07 +0100

    update

--- src/main/java/Audi.java
+++ src/main/java/Audi.java
@@ -1,17 +1,16 @@
 class Car {
-    void run()
-    {
+    void run() {
         System.out.println("car is running");
     }
 }
+
 public class Audi extends Car {
-    void run()
-    {
+    void run() {
         System.out.println("Audi is running safely with 100km");
     }
-    public static void main(String args[])
-    {
-        Car b= new Audi();    //upcasting
+
+    public static void main(String args[]) {
+        Car b = new Audi();    //upcasting
         b.run();
     }
 }
